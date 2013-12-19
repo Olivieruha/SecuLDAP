b8ccc11e6586895ca24f1240e1584a8986a2268b
jonathan.rubiero <jonathan.rubiero@outlook.com>
2013-12-19 11:35:40 +0100

    Ajout controleur admin

--- SecuLDAP/src/main/java/com/miage/SecuLDAP/controller/HomeController.java
+++ SecuLDAP/src/main/java/com/miage/SecuLDAP/controller/HomeController.java
@@ -13,7 +13,7 @@ public class HomeController {
 
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
-		return new ModelAndView("home");
+		return new ModelAndView("admin");
 	}
 	
 	@RequestMapping(value="/helpdesk")
